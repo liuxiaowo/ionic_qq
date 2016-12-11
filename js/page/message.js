987281b10e6b5974d9aa37b85dfcf40d697fb10a
liuxiaowo <1763841230@qq.com>
2016-12-11 23:29:39 +0800

    message page title

--- js/page/message.js
+++ js/page/message.js
@@ -1,4 +1,12 @@
 ﻿//首頁
 myApp.controller('messageCtrl', ['$scope', function ($scope) {
-	
+	var flag = true;
+	$scope.clickbtn = function(id){
+		if(id==1){
+			id.checked = flag;
+		}else{
+			id.checked = !flag;
+		}
+    	flag = !flag;
+	};
 }]);
